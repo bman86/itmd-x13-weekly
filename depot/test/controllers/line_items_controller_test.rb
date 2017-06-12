865b34739f2186b42c52e1438c630939aa1396c8
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-12 18:27:44 -0500

    Implemented empty-cart function

--- depot/test/controllers/line_items_controller_test.rb
+++ depot/test/controllers/line_items_controller_test.rb
@@ -23,9 +23,6 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
       assert_select 'h2', 'Your Pragmatic Cart'
       assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
-    end
-
-    assert_redirected_to line_item_url(LineItem.last)
   end
 
   test "should show line_item" do
@@ -39,7 +36,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item),
+      params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
