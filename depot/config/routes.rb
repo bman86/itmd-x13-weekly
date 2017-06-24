49187bb4c0083d9af978ca952979c05982b571a8
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-06-24 16:09:47 -0500

    Added route for omni

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :carts
   root 'store#index', as: 'store_index'
   resources :products
+  post 'auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
